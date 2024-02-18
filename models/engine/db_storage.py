ee068ada602d5c8e23c43838aac2619517cf8dab
zikotan <tananzakaria630@gmail.com>
2024-02-18 18:34:26 +0100

    response

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,7 +3,6 @@
 from os import getenv
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import (create_engine)
-from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import Base
 from models.state import State
 from models.city import City
