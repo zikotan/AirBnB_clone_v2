433d29256d801cc07ca75c59cca4bd0431f318cd
zikotan <tananzakaria630@gmail.com>
2024-02-18 17:50:59 +0100

    response

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,16 +9,19 @@ class FileStorage:
     __objects = {}
 
     def all(self, cls=None):
-        """returning a dictionary of __object"""
-        myDic = {}
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
+        """
+        dic = {}
         if cls:
-            theDic = self.__objects
-            for key in theDic:
-                myPart = key.replace('.', ' ')
-                myPart = shlex.split(myPart)
-                if (myPart[0] == cls.__name__):
-                    myDic[key] = self.__objects[key]
-            return (myDic)
+            dictionary = self.__objects
+            for key in dictionary:
+                partition = key.replace('.', ' ')
+                partition = shlex.split(partition)
+                if (partition[0] == cls.__name__):
+                    dic[key] = self.__objects[key]
+            return (dic)
         else:
             return self.__objects
 
