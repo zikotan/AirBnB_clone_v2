67d3b899b6d3b94ce7f5b4509cefdee71a4cf7e4
zikotan <tananzakaria630@gmail.com>
2024-02-18 23:18:35 +0100

    response

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -56,7 +56,7 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
