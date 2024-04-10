cc5ed84ad25c3c1f604db33b6a28af4c8a24a023
root <tananzakaria630@gmail.com>
2024-04-09 23:57:29 -0700

    response

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['54.89.109.87', '100.25.190.21']
+env.hosts = ['54.236.54.61', '34.224.17.16']
 
 
 def do_deploy(archive_path):
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -9,7 +9,7 @@ execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['34.239.248.211', '18.234.107.30']
+env.hosts = ['54.236.54.61', '34.224.17.16']
 
 
 def do_pack():
